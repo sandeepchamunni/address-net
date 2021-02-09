911427698b00f73f557ffaa8121b3b6ef7d5e87c
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2021-02-09 21:02:47 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -8,5 +8,5 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/extractaddress", methods=["GET","POST"])
 @cross_origin()
 def extractaddress():
-    return(predict_one("casa del gelato, 10A 24-26 high street road mount waverley vic 3183"))
-    #return("Hello")
+    #return(predict_one("casa del gelato, 10A 24-26 high street road mount waverley vic 3183"))
+    return("Hello")
