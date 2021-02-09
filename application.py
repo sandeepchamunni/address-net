68c2bc0794e94def0ff9532425b277e34d29d389
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2021-02-09 19:59:31 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS, cross_origin
-#from addressnet.predict import predict_one
+from addressnet.predict import predict_one
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -8,5 +8,5 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/extractaddress", methods=["GET","POST"])
 @cross_origin()
 def extractaddress():
-    #return(predict_one("casa del gelato, 10A 24-26 high street road mount waverley vic 3183"))
-    return("Hello")
+    return(predict_one("casa del gelato, 10A 24-26 high street road mount waverley vic 3183"))
+    #return("Hello")
